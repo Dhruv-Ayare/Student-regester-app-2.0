ccac54b97f400912b8fa82e594237847647196b9
Dhruv-Ayare <81720288+Dhruv-Ayare@users.noreply.github.com>
2021-09-15 20:57:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+name_of_the_student_array=[];
+function submit(){
+    var display_student_array=[];
+    for(var j=1;j<=4;j++){
+        var names=document.getElementById("name_of_the_student_"+j).value;
+        console.log(names);
+        name_of_the_student_array.push(names);
+    }
+    console.log(name_of_the_student_array);
+    var length=name_of_the_student_array.length;
+    console.log(length);
+    for(var k=0;k<length;k++){
+        display_student_array.push("<h4>Name-"+name_of_the_student_array[k]+"</h4>");
+        console.log(display_student_array);
+    }
+    document.getElementById("display_name_with_commas").innerHTML=display_student_array;
+    var remove_commas=display_student_array.join("");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML=remove_commas;
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("sort_button").style.display="inline-block";
+}
+function sorting(){
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+    var display_student_array_sorting=[];
+    var length=name_of_the_student_array.length;
+    console.log(length);
+    for(var k=0;k<length;k++){
+        display_student_array_sorting.push("<h4>Name-"+name_of_the_student_array[k]+"</h4>");
+        console.log(display_student_array_sorting);
+}
+var remove_commas=display_student_array_sorting.join("");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML=remove_commas;
+}
